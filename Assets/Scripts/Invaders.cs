821334866d60e1f9950818906dce975b11e29f72
Kooler FAN <koolerkx@gmail.com>
2023-11-23 21:30:32 +0800

    Implement left and right movement

--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -7,6 +7,9 @@ public class Invaders : MonoBehaviour
     private float step = 2f;
     public Invader[] prefabs;
 
+    private Vector3 _direction = Vector3.left;
+    public float speed = 1f;
+
     private void Awake()
     {
         for (int row = 0; row < rows; row++)
@@ -25,6 +28,26 @@ public class Invaders : MonoBehaviour
         }
 
         float offsetX = cols * 1.5f / 2;
-        this.transform.position = new Vector3(- offsetX + step / 2, this.transform.position.y);
+        this.transform.position = new Vector3(-offsetX + step / 2, this.transform.position.y);
+    }
+
+    private void Update()
+    {
+        this.transform.position += _direction * speed * Time.deltaTime;
+
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        foreach (Transform invader in this.transform)
+        {
+            if (_direction == Vector3.right && invader.position.x >= rightEdge.x - 1f)
+            {
+                _direction = Vector3.left;
+            }
+            else if (_direction == Vector3.left && invader.position.x <= leftEdge.x + 1f)
+            {
+                _direction = Vector3.right;
+            }
+        }
     }
 }
