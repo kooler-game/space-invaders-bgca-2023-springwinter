26a0fa831293cdc7b491021eef482e8cebd22b9a
Kooler FAN <koolerkx@gmail.com>
2023-12-07 21:34:23 +0800

    Implement restart

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Invaders grid;
+
+    public void Update() {
+        if(grid.isEmpty()) {
+            grid.spawn();
+        }
+    }
+}
--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -12,23 +12,7 @@ public class Invaders : MonoBehaviour
 
     private void Awake()
     {
-        for (int row = 0; row < rows; row++)
-        {
-            float positionY = row * step;
-            for (int col = 0; col < cols; col++)
-            {
-                float positionX = col * step;
-                Vector3 position = new Vector3(positionX, positionY);
-
-                // int prefabsType = (int)(Random.value * prefabs.Length);
-                // Invader invader = Instantiate(prefabs[prefabsType], this.transform);
-                Invader invader = Instantiate(prefabs[row], this.transform);
-                invader.transform.localPosition = position;
-            }
-        }
-
-        float offsetX = cols * 1.5f / 2;
-        this.transform.position = new Vector3(-offsetX + step / 2, this.transform.position.y);
+        spawn();
     }
 
     private void Update()
@@ -55,5 +39,36 @@ public class Invaders : MonoBehaviour
     {
         _direction.x *= -1;
         this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 1);
+    }
+
+    public void spawn()
+    {
+        for (int row = 0; row < rows; row++)
+        {
+            float positionY = row * step;
+            for (int col = 0; col < cols; col++)
+            {
+                float positionX = col * step;
+                Vector3 position = new Vector3(positionX, positionY);
+
+                // int prefabsType = (int)(Random.value * prefabs.Length);
+                // Invader invader = Instantiate(prefabs[prefabsType], this.transform);
+                Invader invader = Instantiate(prefabs[row], this.transform);
+                invader.transform.localPosition = position;
+            }
+        }
+
+        float offsetX = cols * 1.5f / 2;
+        this.transform.position = new Vector3(-offsetX + step / 2, this.transform.position.y);
+    }
+
+    public bool isEmpty() {
+        foreach(Transform invader in this.transform) {
+            if(invader.gameObject.activeSelf) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
